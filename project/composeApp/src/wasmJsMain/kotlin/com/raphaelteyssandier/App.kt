4c9eb5da6f9657e100338d6baaa41d61ed26d223
doTTTTT <raphael.teyssandier@epitech.eu>
2024-10-20 18:37:47 +0200

    feature: Rework

--- project/composeApp/src/wasmJsMain/kotlin/com/raphaelteyssandier/App.kt
+++ project/composeApp/src/wasmJsMain/kotlin/com/raphaelteyssandier/App.kt
@@ -3,6 +3,7 @@ package com.raphaelteyssandier
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Button
@@ -12,14 +13,14 @@ import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Abc
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
+import androidx.compose.material3.adaptive.navigationsuite.*
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults.calculateFromAdaptiveInfo
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldLayout
-import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.window.core.layout.WindowWidthSizeClass
 import kotlinx.browser.window
 import org.jetbrains.compose.resources.painterResource
@@ -45,24 +46,30 @@ fun Content() {
         }
     }
 
-    NavigationSuiteScaffold(
-        navigationSuiteItems = {
-            item(
-                selected = false,
-                onClick = {},
-                icon = {
-                    Icon(
-                        imageVector = Icons.Filled.Abc,
-                        contentDescription = null
-                    )
-                },
-                label = {
-                    Text("Raphael ?")
-                }
-            )
+    NavigationSuiteScaffoldLayout(
+        navigationSuite = {
+            NavigationSuite(
+                layoutType = layoutType,
+                modifier = Modifier
+                    .shadow(8.dp)
+                    .fillMaxHeight()
+            ) {
+                item(
+                    selected = false,
+                    onClick = {},
+                    icon = {
+                        Icon(
+                            imageVector = Icons.Filled.Abc,
+                            contentDescription = null
+                        )
+                    },
+                    label = {
+                        Text("Raphael ?")
+                    }
+                )
+            }
         },
-        layoutType = layoutType,
-        modifier = Modifier.fillMaxSize()
+        layoutType = layoutType
     ) {
         Test()
     }
